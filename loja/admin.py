d6bc08b5e5c616ef96e8461b8768f47faee3666f
Artur Cortez Souza da Silva <20221011110013@ifrn.local>
2024-04-01 08:15:38 -0300

    Slides 04

--- loja/admin.py
+++ loja/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 
 from .models import * 
-admin.site.register(Fabricante) 
+
+class FabricanteAdmin(admin.ModelAdmin):
+    date_hierarchy = 'criado_em'
+
+class ProdutoAdmin(admin.ModelAdmin):
+    date_hierarchy = 'criado_em'
+    list_display = ('produto', 'destaque', 'promocao', 'msgPromocao', 'preco', 'categoria',)
+    empty_value_display = 'Vazio'
+    search_fields = ('produto',)
+    exclude = ('msgPromocao',)
+
+admin.site.register(Fabricante, FabricanteAdmin) 
 admin.site.register(Categoria) 
-admin.site.register(Produto) 
+admin.site.register(Produto, ProdutoAdmin) 
