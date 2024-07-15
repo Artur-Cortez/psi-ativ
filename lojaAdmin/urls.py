235ae81a0f7494b243ae7d7f2f27b83dd85e9e13
Artur Cortez Souza da Silva <20221011110013@ifrn.local>
2024-07-15 07:33:23 -0300

    capitulo 5

--- lojaAdmin/urls.py
+++ lojaAdmin/urls.py
@@ -4,9 +4,12 @@ from django.conf import settings
 
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('loja.urls.HomeUrls')),
+    path('produto/', include('loja.urls.ProdutoUrls')),
 ] 
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
