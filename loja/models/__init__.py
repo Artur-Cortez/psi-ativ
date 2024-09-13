ed0a252ce8f75bc43fe6a3a961c26671dedc86b3
Artur-Cortez <souza.cortez.013@gmail.com>
2024-09-13 10:27:32 -0300

    Início do capítulo 08

--- loja/models/__init__.py
+++ loja/models/__init__.py
@@ -1,9 +1,17 @@
+
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-
 from .fabricante import Fabricante
 from .produto import Produto
 from .categoria import Categoria
+
+PERFIL = (
+    (1, 'Admin'),
+    (2, 'Usuario')
+)
+
+from .usuario import Usuario
+
